07710630547faa046fb240b0eb8f7b5476045053
thiago alves <thiago87gsa@gmail.com>
2024-10-28 11:20:37 -0300

    :sparkles: feat: add metodo update no seriazisers

--- api/serializers.py
+++ api/serializers.py
@@ -21,3 +21,20 @@ class ItemSerializer(serializers.ModelSerializer):
         category, created = Category.objects.get_or_create(**category_data)
         item = Item.objects.create(category=category, **validated_data)
         return item
+
+    def update(self, instance, validated_data):
+        # Extrai e remove os dados da categoria do validated_data
+        category_data = validated_data.pop('category', None)
+
+        # Atualiza os campos do item
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+
+        # Lida com a categoria aninhada
+        if category_data:
+            category_name = category_data.get('name')
+            # Busca a categoria ou cria uma nova com o nome fornecido
+            category, created = Category.objects.get_or_create(name=category_name)
+            instance.category = category
+        instance.save()
+        return instance
--- meu_projeto/settings.py
+++ meu_projeto/settings.py
@@ -78,7 +78,7 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2,  # Define o número de itens por página
+    'PAGE_SIZE': 4,  # Define o número de itens por página
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
